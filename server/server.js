4c2c6790204f91cb1648856946c2ddfd7e3ad86b
ComdevX <comdevx@gmail.com>
2017-02-06 18:20:54 +0700

    add code  for insert Codes for test

--- server/server.js
+++ server/server.js
@@ -18,3 +18,21 @@ Refills.allow({
         return true;
     },
 });
+
+Codes.allow({
+    insert: function (userId, doc) {
+        return true;
+    },
+});
+
+if (Codes.find().count() === 0) {
+    Codes.insert({
+        code: '123', price: 100, createdAt: new Date()
+    });
+    Codes.insert({
+        code: '456', price: 1000, createdAt: new Date()
+    });
+    Codes.insert({
+        code: '789', price: 10000, createdAt: new Date()
+    });
+}
